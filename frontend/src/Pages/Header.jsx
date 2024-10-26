bae38837616a252bf07cc544b23507f035ef6e71
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-10-26 18:29:38 +0545

    header jsx file

--- frontend/src/Pages/Header.jsx
+++ frontend/src/Pages/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  let navigate = useNavigate();
   return (
     <div className="header">
-      <p>Linux Website</p>
+      <p onClick={() => (navigate = "/")} style={{ cursor: "pointer" }}>
+        Linux Website
+      </p>
       <div className="link-container">
         <Link to="/">Home</Link>
         <Link to="/">Getting Started</Link>
       </div>
       <form>
+        <span>Search here:</span>
         <input type="text" name="" value="" />
         <button>Search</button>
       </form>
